17a5be27fb284977419776a8a143bbe8c3aa715f
wesleyhjpr <wesleyhjpr@gmail.com>
2022-02-19 22:05:21 -0300

    add middleware to set content-type: application/json into header

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"golang-api-rest/controllers"
+	"golang-api-rest/middleware"
 	"log"
 	"net/http"
 
@@ -10,6 +11,7 @@ import (
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalities", controllers.AllPersonalities).Methods("Get")
 	r.HandleFunc("/api/personalities/{id}", controllers.GetPersonalityById).Methods("Get")
